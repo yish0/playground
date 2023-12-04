702be4ed680927788010e1797745e32832c6d80a
yish0 <ca01952@cartahd.com>
2023-12-04 17:18:56 +0900

    add subnets and internet gateway

--- aws/main.tf
+++ aws/main.tf
@@ -24,3 +24,71 @@ resource "aws_subnet" "name" {
     Name = "handson-public-1a"
   }
 }
+
+resource "aws_subnet" "public_1c" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1c"
+
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "handson-public-1c"
+  }
+}
+
+resource "aws_subnet" "public_1d" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1d"
+
+  cidr_block = "10.0.3.0/24"
+
+  tags = {
+    Name = "handson-public-1d"
+  }
+}
+
+# Private Subnets
+resource "aws_subnet" "private_1a" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1a"
+  cidr_block        = "10.0.10.0/24"
+
+  tags = {
+    Name = "handson-private-1a"
+  }
+}
+
+resource "aws_subnet" "private_1c" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1c"
+  cidr_block        = "10.0.20.0/24"
+
+  tags = {
+    Name = "handson-private-1c"
+  }
+}
+
+resource "aws_subnet" "private_1d" {
+  vpc_id = aws_vpc.main.id
+
+  availability_zone = "ap-northeast-1d"
+  cidr_block        = "10.0.30.0/24"
+
+  tags = {
+    Name = "handson-private-1d"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+
+  tags = {
+    Name = "handson"
+  }
+
+}
