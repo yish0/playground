2aa3c1b8e89da2c103dfa9a882aaaf604249d803
yish0 <ca01952@cartahd.com>
2024-02-01 17:18:02 +0900

    done

--- practice/cleancraftmanship/src/bowling/Bowling.test.ts
+++ practice/cleancraftmanship/src/bowling/Bowling.test.ts
@@ -3,15 +3,59 @@ import { Game } from "./Game"
 
 var g: Game = new Game()
 
-test("nothing", () => {})
+function setUp() {
+    g = new Game()
+}
+
+function rollMany(n: number, pins: number) {
+    for (var i: number = 0; i < n; i++) {
+        g.roll(pins)
+    }
+}
+
+function rollSpare() {
+    rollMany(2, 5)
+}
+
+function rollStrike() {
+    g.roll(10)
+}
 
 test("can create game", () => {
     var g: Game = new Game()
 })
 
-test("can roll", () => {
-    expect(() => {
-        var g: Game = new Game()
-        g.roll(0)
-    }).toThrow(Error)
+test("gutter game", () => {
+    setUp()
+    rollMany(20, 0)
+    expect(g.getScore()).toBe(0)
+})
+
+test("all ones", () => {
+    setUp()
+    rollMany(20, 1)
+    expect(g.getScore()).toBe(20)
+})
+
+test("one spare", () => {
+    setUp()
+    rollSpare()
+    g.roll(7)
+    rollMany(17, 0)
+    expect(g.getScore()).toBe(24)
+})
+
+test("one strike", () => {
+    setUp()
+    rollStrike()
+    g.roll(2)
+    g.roll(3)
+    rollMany(16, 0)
+    expect(g.getScore()).toBe(20)
+})
+
+test("perfect game", () => {
+    setUp()
+    rollMany(12, 10)
+    expect(g.getScore()).toBe(300)
 })
--- practice/cleancraftmanship/src/bowling/Game.ts
+++ practice/cleancraftmanship/src/bowling/Game.ts
@@ -1,3 +1,49 @@
 export class Game {
-    public roll(pins: number): void {}
+    private rolls: number[] = []
+
+    private currentRoll: number = 0
+
+    public roll(pins: number): void {
+        this.rolls[this.currentRoll++] = pins
+    }
+
+    public getScore(): number {
+        var score: number = 0
+        var i = 0
+        var frameIndex = 0
+        for (var frame: number = 0; frame < 10; frame++) {
+            if (this.isStrike(frameIndex)) {
+                score += 10 + this.strikeBonus(frameIndex)
+                frameIndex++
+            } else if (this.isSpare(frameIndex)) {
+                // spare
+                score += 10 + this.spareBonus(frameIndex)
+                frameIndex += 2
+            } else {
+                score += this.twoBallsInFrame(frameIndex)
+                frameIndex += 2
+            }
+        }
+        return score
+    }
+
+    private isSpare(frameIndex: number): boolean {
+        return this.rolls[frameIndex] + this.rolls[frameIndex + 1] == 10
+    }
+
+    private isStrike(frameIndex: number): boolean {
+        return this.rolls[frameIndex] == 10
+    }
+
+    private strikeBonus(frameIndex: number): number {
+        return this.rolls[frameIndex + 1] + this.rolls[frameIndex + 2]
+    }
+
+    private spareBonus(frameIndex: number): number {
+        return this.rolls[frameIndex + 2]
+    }
+
+    private twoBallsInFrame(frameIndex: number): number {
+        return this.rolls[frameIndex] + this.rolls[frameIndex + 1]
+    }
 }
